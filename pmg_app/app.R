269af2753dc62c05e51aaa1ef3cf9a15c074a4b4
Pedro Martín <pmargom@gmail.com>
2018-04-27 19:05:57 +0100

    imagenes, estilos, tags html

--- pmg_app/app.R
+++ pmg_app/app.R
@@ -1,11 +1,41 @@
 library(shiny)
+library(markdown)
 
-ui <- fluidPage({
-  'hola'
-})
+ui <- fluidPage(
+  #includeMarkdown('fichero.md')
+  #tags$head(
+  #  tags$link(rel = "stylesheet", type = "text/css", href = "./css/styles.css")
+  #)  
+  #tags$div(
+  #  tags$p('es un párrafo'),
+    #tags$img(src='./images/coche.jpg', alt='mi coche', style='border: solid 1px green')
+  #)
+  checkboxInput("myCheckbox",
+                label = "Choice A",
+                value = TRUE),
+  textOutput("myId"),
+  radioButtons(
+    'myRB',
+    'Elije un número',
+    choices = list(
+      'el numero 1' = 1,
+      'el numero 2' = 2,
+      'el numero 3' = 3
+    )
+  )
+
+)
 
 server <- function(input, output) {
-  
+  output$myId <- renderText(
+    'hola caracola'
+  )
+  output$myCheckbox <- renderText(
+    input$myCheckbox
+  )
+  output$myRB <- renderText(
+    input$myRB
+  )
 }
 
 #kaskjas
