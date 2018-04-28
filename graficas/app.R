5c793630ef49500e7e1aeb7b4db821a419c0f7f2
Pedro Martin <pmargom@gmail.com>
2018-04-28 19:12:34 +0100

    filtrado por fechas

---
+++ graficas/app.R
@@ -0,0 +1,65 @@
+
+library(shiny)
+
+ui <- fluidPage(
+  
+  # selectInput("listaValores",
+  #             label = h3("Elije un valor"), 
+  #             choices = c())
+  
+  selectizeInput("listaValores",
+                 label = h3("Elije un valor"), 
+                 choices = c()),
+  
+  selectInput("tipo.accidente",
+              label = h3("Elije un valor"),
+              choices = c()),
+
+  
+  #plotOutput('histograma'),
+  
+  dateRangeInput("fechas",
+                 label = h3("Rango de fechas"),
+                 start = '01/01/2018',
+                 format = 'dd/mm/yyyy',
+                 language = 'es'
+  ),
+
+  tableOutput('table.accidentes')
+  
+  
+)
+
+server <- function(input, output, session) {
+  data <- read.csv(
+    file='bicicletas.csv',
+    header=TRUE,
+    fileEncoding = 'latin1',
+    sep=';'
+  )
+  
+  data$Fecha <- as.Date(data$Fecha, '%d/%m/%Y') #convierto al fecha en string a fecha de verdad
+  data$Lugar <- trimws(data$Lugar)
+  data$FechaOK <- format(data$Fecha, '%d/%m/%y')
+  
+  observe({
+    #updateSelectInput(session, 'listaValores', choices = sort(data$DISTRITO)) 
+    updateSelectizeInput(session, 'listaValores', choices = sort(data$DISTRITO)) #ddl editable
+  })
+  
+  observe({
+    updateSelectInput(session, 'tipo.accidente', choices = sort(data[data$DISTRITO == input$listaValores, ]$Tipo.Accidente))
+  })
+  
+  # output$histograma <- renderPlot({
+  #   hist(data$Nm.Tot.Victimas)
+  # })
+  
+  output$table.accidentes <- renderTable(
+    data[data$DISTRITO == input$listaValores & data$Tipo.Accidente == input$tipo.accidente & data$Fecha >= input$fechas[1] & data$Fecha <= input$fechas[2], ]
+  )
+  
+  
+}
+
+shinyApp(ui, server)
